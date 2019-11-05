49a2923decdfd2bef5bd80f431de31212f097b1d
cnnrbeck <cnnrbeck@gmail.com>
2019-11-05 13:06:54 -0700

    got the api data!

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-async function getMTGData() {
-    const response = await fetch('https://api.magicthegathering.io/v1/cards')
-    const myJson = await response.json()
-    console.log(JSON.stringify(myJson))
+async function getMTGData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    }
+    catch(error) {
+        console.error(error)
+    }
 }
 
-getMTGData()
+const theData = getMTGData('https://api.magicthegathering.io/v1/cards/')
+.then(data => {
+    let cardArr = data.cards;
+    console.log(cardArr)
+    for(const card of cardArr)
+    {
+        console.log(card)
+    }
+})
+
+console.log(theData)
+
+
+// let card = getMTGData()
+// console.log(card)
+// let mainArea = document.querySelector('main')
+
+// let name = document.createElement('h1')
+// let manaCost = document.createElement('h3')
+
+// name.textContent = card.name
+// manaCost.textContent = card.manaCost
+// console.log(name)
+// console.log(manaCost)
+
+// mainArea.appendChild(name)
+// mainArea.appendChild(manaCost)
